5a9988e78693fc638c683286d25c8699665a763a
adityahimaone <adityahimaone@gmail.com>
2024-01-22 22:57:56 +0700

    feat(subscribe): add section subscribe and add custom prop button

--- src/lib/components/ui/Button.tsx
+++ src/lib/components/ui/Button.tsx
@@ -7,14 +7,22 @@ type Props = {
   iconUrl?: string;
   onClick?: () => void;
   variant?: 'primary' | 'secondary';
+  fullWidth?: boolean;
 };
 
-const Button = ({ label, iconUrl, onClick, variant = 'primary' }: Props) => {
+const Button = ({
+  label,
+  iconUrl,
+  onClick,
+  variant = 'primary',
+  fullWidth = false,
+}: Props) => {
   const buttonStyles = cn(
-    'flex items-center justify-center gap-2 rounded-full border text-lg leading-none px-7 py-4',
+    'flex items-center justify-center gap-2 rounded-full border px-7 py-4 text-lg leading-none',
     {
       'bg-coral-red text-white': variant === 'primary',
-      'bg-white border-slate-gray text-slate-gray': variant === 'secondary',
+      'border-slate-gray bg-white text-slate-gray': variant === 'secondary',
+      'w-full': fullWidth,
     }
   );
 
--- src/lib/pages/home/Sections/Subscribe.tsx
+++ src/lib/pages/home/Sections/Subscribe.tsx
@@ -1,6 +1,27 @@
+import Button from '@/lib/components/ui/Button';
+
 const Subscribe = () => {
   return (
-    <section className="padding-x w-full py-16 sm:py-32">Subscribe</section>
+    <section id="contact-us" className="padding-x w-full py-16 sm:py-32">
+      <div className="max-container flex items-center justify-between gap-10 max-lg:flex-col">
+        <h3 className="font-palanquin text-4xl font-bold leading-[68px] lg:max-w-md">
+          Sign Up from <span className="text-coral-red">Updates</span> &
+          Newsletter
+        </h3>
+        <div className="flex w-full items-center gap-5 rounded-full p-2.5 max-sm:flex-col sm:border sm:border-slate-gray lg:max-w-[40%]">
+          <input
+            type="text"
+            name="email"
+            id="email-newsletter"
+            placeholder="subscribe@nike.com"
+            className="input"
+          />
+          <div className="flex items-center max-sm:w-full max-sm:justify-end">
+            <Button label="Sign Up" fullWidth />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
